8089a3960f05fac728fabc20ef3bc1ac5490437d
gabriel <vieyra.gabriel08@gmail.com>
2021-06-03 11:55:28 -0300

    ajustes en el carousel

--- frontend/src/components/Slider/Slider.js
+++ frontend/src/components/Slider/Slider.js
@@ -1,45 +1,49 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import imgBadBoys from "../../assets/images/bad-boys.jpg";
+import imgUnidos from "../../assets/images/unidos.jpg";
+import imgSonic from "../../assets/images/sonic.jpg";
+import imgAvesDePresa from "../../assets/images/aves-presa.jpg";
 
 const Slider = () => {
+    const dataCarousel = [
+        {
+            title: "Bad Boys",
+            image: imgBadBoys,
+            id: 1
+        },
+        {
+            title: "Unidos",
+            image: imgUnidos,
+            id: 2
+        },
+        {
+            title: "Sonic",
+            image: imgSonic,
+            id: 3
+        },
+        {
+            title: "Aves de presa",
+            image: imgAvesDePresa,
+            id: 4
+        }
+    ];
+
     return (
         <Carousel className="mb-2">
-           <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="images/bad-boys.jpg"
-                        alt="First Slide"
-                    />
-           </Carousel.Item>
-
-           <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="images/unidos.jpg"
-                        alt="Second Slide"
-                    />
-           </Carousel.Item>
-
-           <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="images/sonic.jpg"
-                        alt="Third Slide"
-                    />
-           </Carousel.Item>
-
-           <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="images/aves-presa.jpg"
-                        alt="Fourth Slide"
-                    />
-           </Carousel.Item>
-        </Carousel> 
-    )
-}
+            {dataCarousel.map((val, key) => {
+                return (
+                    <Carousel.Item key={val.id ? val.id : key}>
+                        <img
+                            className="d-block w-100"
+                            src={val.image}
+                            alt={`imagen de la pelicula ${val.title}`}
+                        />
+                    </Carousel.Item>
+                );
+            })}
+        </Carousel>
+    );
+};
 
 export default Slider;
-
-        
-
